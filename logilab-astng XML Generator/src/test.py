e21aede1b37ad746572c9d6030aaf826224d71af
bluesbreaker <bluesbreaker@01ffd6c0-fe64-4d5d-bb8c-b9d327b80048>
2011-08-25 16:54:18 +0000

    Расширл набор обрабатываемых нодов, добавив XML-атрибуты "на глазок".
    git-svn-id: svn://127.0.0.1@3 01ffd6c0-fe64-4d5d-bb8c-b9d327b80048

--- logilab-astng XML Generator/src/test.py
+++ logilab-astng XML Generator/src/test.py
@@ -19,6 +19,10 @@ if __name__ == '__main__':
 def make_tree(root_xml,root_astng):
     ''' Create tag with name of node class'''
     current_xml_node = etree.Element(root_astng.__class__.__name__)
+    '''If node, related with position in text file'''
+    if(hasattr(root_astng, 'fromlineno')):
+        '''FIXME Module doesn't need it''' 
+        current_xml_node.set("fromlineno",str(root_astng.fromlineno))
     if(isinstance(root_astng, Import)):
         for name in root_astng.names:
             sub = etree.Element("ImportName", name=name[0])
@@ -36,21 +40,55 @@ def make_tree(root_xml,root_astng):
         current_xml_node.set("name",root_astng.name)
     elif(isinstance(root_astng, AssName)):
         current_xml_node.set("name",root_astng.name)
+    elif(isinstance(root_astng, DelName)):
+        ##What is it?
+        current_xml_node.set("unknown","true")
+    elif(isinstance(root_astng, Arguments)):
+        '''Represent format of arguments'''
+        current_xml_node.set("format_args",root_astng.format_args())
+    elif(isinstance(root_astng, AssAttr)):
+        current_xml_node.set("attrname",root_astng.attrname)
+        current_xml_node.set("expr",root_astng.expr.as_string())
+    elif(isinstance(root_astng, Assert)):
+        #Type of assert (what is checked?). It may be just name or compare
+        current_xml_node.set("class",root_astng.test.__class__.__name__)
+        current_xml_node.set("test",root_astng.test.as_string())
+        #current_xml_node.set("expr",root_astng.expr.as_string())
     elif(isinstance(root_astng, Assign)):
+        #FIXME better targets
         current_xml_node.set("targets",str(root_astng.targets))
-        current_xml_node.set("value",str(root_astng.value))
+        current_xml_node.set("value",root_astng.value.as_string())
+    elif(isinstance(root_astng, AugAssign)):
+        #+= Assign
+        current_xml_node.set("target",root_astng.target.as_string())
+        current_xml_node.set("value",root_astng.value.as_string())
+    elif(isinstance(root_astng, Backquote)):
+        # It is ` command call, but i heven't seen it yet
+        current_xml_node.set("value",root_astng.value.as_string())
+    elif(isinstance(root_astng, BinOp)):
+        # Binary operation % or >>, for example
+        current_xml_node.set("op",root_astng.op)
+        current_xml_node.set("left",root_astng.left.as_string())
+        current_xml_node.set("right",root_astng.right.as_string())
+    elif(isinstance(root_astng, BoolOp)):
+        #may be FIXME - is not beautiful
+        str_val = ""
+        for val in root_astng.values:
+            str_val+= val.as_string()+',' 
+        current_xml_node.set("op",root_astng.op)
+        current_xml_node.set("values",str_val)
+    elif(isinstance(root_astng, Break)):
+        #isn't very interesting
+        pass
+    elif(isinstance(root_astng, CallFunc)):
+        current_xml_node.set("func",str(root_astng.func))
+    elif(isinstance(root_astng, Compare)):
+        #0 of ops is operation, !=, for example
+        #FIXME ops is lisr of tuples!!!!!
+        current_xml_node.set("op",str(root_astng.ops[0]))
     elif(isinstance(root_astng, Const)):
         current_xml_node.set("type",root_astng.name)
         current_xml_node.text = str(root_astng.as_string())
-    elif(isinstance(root_astng, CallFunc)):
-        #root_astng.__class__.__name__
-        print ####
-        print root_astng.starargs
-        print root_astng.args
-        print root_astng.func.__class__.__name__
-        print root_astng.kwargs
-        current_xml_node.set("func",str(root_astng.func))
-        #current_xml_node.text = str(root_astng.as_string())
     elif(isinstance(root_astng, Name)):
         current_xml_node.set("name", root_astng.name)
     elif(isinstance(root_astng, Getattr)):
