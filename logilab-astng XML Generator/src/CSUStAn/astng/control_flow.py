7ef7c2f97d02457a7ca8d7dc3bc07b89e4d43914
bluesbreaker <slayer-gurgen@yandex.ru>
2013-04-15 01:01:22 +0600

    CFGExtractor: добавил поддержку циклов и разделил условия(условия циклов) условных вершин(циклов) и их тела. Также по функции теперь выводятся блоки только из ее тела

--- logilab-astng XML Generator/src/CSUStAn/astng/control_flow.py
+++ logilab-astng XML Generator/src/CSUStAn/astng/control_flow.py
@@ -5,7 +5,7 @@ Created on 14.04.2013
 '''
 from logilab.astng.utils import LocalsVisitor
 from logilab.astng.inspector import IdGeneratorMixIn
-from logilab.astng.node_classes import If
+from logilab.astng.node_classes import If, For, While
 import pydot
 
 class CFGLinker(IdGeneratorMixIn, LocalsVisitor):
@@ -22,8 +22,6 @@ class CFGLinker(IdGeneratorMixIn, LocalsVisitor):
             return
         graph = pydot.Dot(graph_type='digraph')
         self.extract(node,graph,0)
-        #children = list(node.get_children())
-        #blocks = [pydot.Node(child.__class__.__name__+' '+str(num),shape='record')for num,child in enumerate(children)]
         graph.write_svg('cfg.svg')
         f=open('cfg.txt','w')
         f.write(node.as_string())
@@ -32,7 +30,7 @@ class CFGLinker(IdGeneratorMixIn, LocalsVisitor):
     def extract(self,node,graph,id_count,parent=None):
         prev=parent
         if_prev = None
-        for child in node.get_children():
+        for child in node.body:
             id_count+=1
             block = pydot.Node(child.__class__.__name__+' '+str(id_count),shape='record')
             graph.add_node(block)
@@ -43,11 +41,31 @@ class CFGLinker(IdGeneratorMixIn, LocalsVisitor):
                 graph.add_edge(edge)
                 prev=block
             if if_prev is not None:
+                ''' merge of flows after if '''
                 edge = pydot.Edge(if_prev,block)
                 graph.add_edge(edge)
             if isinstance(child, If):
-                self._stop = True
+                test = pydot.Node(child.test.__class__.__name__+' '+str(id_count))
+                graph.add_node(test)
+                edge = pydot.Edge(block,test,dir='none',style='dashed')
+                graph.add_edge(edge)
                 id_count, if_prev = self.extract(child, graph, id_count, block)
+            elif isinstance(child, (For, While)):
+                if isinstance(child, For):
+                    iterate = pydot.Node(child.iter.__class__.__name__+' '+str(id_count))
+                    graph.add_node(iterate)
+                    edge = pydot.Edge(block,iterate,dir='none',style='dashed')
+                    graph.add_edge(edge)
+                else:
+                    self._stop = True
+                    test = pydot.Node(child.test.__class__.__name__+' '+str(id_count))
+                    graph.add_node(test)
+                    edge = pydot.Edge(block,test,dir='none',style='dashed')
+                    graph.add_edge(edge)
+                id_count, cycle_prev = self.extract(child, graph, id_count, block)
+                ''' cycle edge '''
+                edge = pydot.Edge(cycle_prev,block)
+                graph.add_edge(edge)
             else:
                 if_prev = None
         return id_count, block
