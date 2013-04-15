19014cd020f6f5c18de9a78e9c3ec73e3c61e21c
bluesbreaker <slayer-gurgen@yandex.ru>
2013-04-15 23:44:44 +0600

    CFGExtractor: Сделал генерацию XML, вывод в dot сделал после получения XML, отделил if,while,for как отдельный тип в графе

--- logilab-astng XML Generator/src/CSUStAn/astng/control_flow.py
+++ logilab-astng XML Generator/src/CSUStAn/astng/control_flow.py
@@ -7,68 +7,106 @@ from logilab.astng.utils import LocalsVisitor
 from logilab.astng.inspector import IdGeneratorMixIn
 from logilab.astng.node_classes import If, For, While
 import pydot
+from lxml import etree
 
 class CFGLinker(IdGeneratorMixIn, LocalsVisitor):
     '''
     classdocs
     '''
-    _stop=False
+    _root = None
+    ''' dbg '''
+    _stop = False
+    _stack = {}
 
     def __init__(self, project):
         IdGeneratorMixIn.__init__(self)
         LocalsVisitor.__init__(self)
+    
+    def visit_project(self,node):
+        self._root = etree.Element("Project")
+    def leave_project(self,node):
+        f = open('cfg.xml','w')
+        f.write(etree.tostring(self._root, pretty_print=True, encoding='utf-8', xml_declaration=True))
+        f.close()
     def visit_function(self,node):
-        if(self._stop or (len(list(node.get_children()))<4)):
+        func_node = etree.Element("Function",name=node.name,label=node.root().name)
+        self._stack[node] = func_node
+        self._root.append(func_node)
+        self.extract(node,func_node,0)
+    def leave_function(self,node):
+        ''' DEBUG '''
+        if self._stop or (len(node.body)<4):
+            del self._stack[node]
             return
         graph = pydot.Dot(graph_type='digraph')
-        self.extract(node,graph,0)
+        block_dict = {}
+        for block in self._stack[node].iter("Block"):
+            dot_node = pydot.Node(block.get("type")+' '+block.get("id"),shape='record')
+            graph.add_node(dot_node)
+            block_dict[block.get("id")] = dot_node
+        for block in self._stack[node].iter("If"):
+            dot_node = pydot.Node('If '+block.get("id")+'\l'+block.get("test"),shape='diamond')
+            graph.add_node(dot_node)
+            block_dict[block.get("id")] = dot_node
+        for block in self._stack[node].iter("For"):
+            dot_node = pydot.Node('For '+block.get("id")+'\l'+block.get("iterate"),shape='diamond')
+            graph.add_node(dot_node)
+            block_dict[block.get("id")] = dot_node
+        for block in self._stack[node].iter("While"):
+            dot_node = pydot.Node('While '+block.get("id")+'\l'+block.get("test"),shape='diamond')
+            graph.add_node(dot_node)
+            block_dict[block.get("id")] = dot_node
+        for flow in self._stack[node].iter("Flow"):
+            dot_edge = pydot.Edge(block_dict[flow.get("from_id")],block_dict[flow.get("to_id")])
+            graph.add_edge(dot_edge)
         graph.write_svg('cfg.svg')
         f=open('cfg.txt','w')
         f.write(node.as_string())
         f.close()
+        self._stop = True
+        del self._stack[node]
         
-    def extract(self,node,graph,id_count,parent=None):
-        prev=parent
+    def extract(self,node,parent_node,id_count):
+        prev=id_count
         if_prev = None
         for child in node.body:
             id_count+=1
-            block = pydot.Node(child.__class__.__name__+' '+str(id_count),shape='record')
-            graph.add_node(block)
-            if prev is None:
-                prev=block
+            if isinstance(child, If):
+                block_node = etree.Element("If",id=str(id_count))
+            elif isinstance(child, For):
+                block_node = etree.Element("For",id=str(id_count))
+            elif isinstance(child, While):
+                block_node = etree.Element("While",id=str(id_count))
             else:
-                edge = pydot.Edge(prev,block)
-                graph.add_edge(edge)
-                prev=block
+                block_node = etree.Element("Block",type=child.__class__.__name__,id=str(id_count))
+            curr_id = id_count
+            parent_node.append(block_node)
+            if prev == 0:
+                prev=id_count
+            else:
+                flow_node = etree.Element("Flow",from_id=str(prev),to_id=str(curr_id))
+                parent_node.append(flow_node)
+                prev=id_count
             if if_prev is not None:
                 ''' merge of flows after if '''
-                edge = pydot.Edge(if_prev,block)
-                graph.add_edge(edge)
+                flow_node = etree.Element("Flow",from_id=str(if_prev),to_id=str(curr_id))
+                parent_node.append(flow_node)
+                if_prev = None
             if isinstance(child, If):
-                test = pydot.Node(child.test.__class__.__name__+' '+str(id_count))
-                graph.add_node(test)
-                edge = pydot.Edge(block,test,dir='none',style='dashed')
-                graph.add_edge(edge)
-                id_count, if_prev = self.extract(child, graph, id_count, block)
+                block_node.set("test",child.test.__class__.__name__)
+                id_count = self.extract(child, parent_node, id_count)
+                if_prev = id_count
             elif isinstance(child, (For, While)):
                 if isinstance(child, For):
-                    iterate = pydot.Node(child.iter.__class__.__name__+' '+str(id_count))
-                    graph.add_node(iterate)
-                    edge = pydot.Edge(block,iterate,dir='none',style='dashed')
-                    graph.add_edge(edge)
+                    block_node.set("iterate",child.iter.__class__.__name__)
                 else:
-                    self._stop = True
-                    test = pydot.Node(child.test.__class__.__name__+' '+str(id_count))
-                    graph.add_node(test)
-                    edge = pydot.Edge(block,test,dir='none',style='dashed')
-                    graph.add_edge(edge)
-                id_count, cycle_prev = self.extract(child, graph, id_count, block)
+                    block_node.set("test",child.test.__class__.__name__)
+                id_count = self.extract(child,parent_node, id_count)
+                cycle_prev = id_count
                 ''' cycle edge '''
-                edge = pydot.Edge(cycle_prev,block)
-                graph.add_edge(edge)
-            else:
-                if_prev = None
-        return id_count, block
+                flow_node = etree.Element("Flow",from_id=str(cycle_prev),to_id=str(curr_id))
+                parent_node.append(flow_node)
+        return id_count
                 
        
         
